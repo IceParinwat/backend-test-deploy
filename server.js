fda35f83bc03f821fe554c51ba9dda479c9b12f6
Ice Kittipong <ammice1122@gmail.com>
2024-12-02 14:54:14 +0700

    edit server core

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-// import { readdirSync } from "fs";
 // Step 2
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
@@ -11,46 +10,50 @@ import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+
 // App Config
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(cors());
+
 // Service connections
 connectDB();
 connectCloudinary();
-// Middlewares
+
+// middlewares
 app.use(express.json());
-// Define the list of allowed origins
-// const allowedOrigins = [
-//   "https://artnakkk-frontend-admin.vercel.app",
-//   "http://localhost:5175", // For local development
-//   "http://localhost:5176", // For local development
-//   "http://localhost:5173", // For local development
-// ];
-// // Configure CORS
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       // Allow requests with no origin (e.g., mobile apps or curl)
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.includes(origin)) {
-//         callback(null, true); // Allow the origin
-//       } else {
-//         callback(new Error("Not allowed by CORS")); // Block the origin
-//       }
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true, // Allow cookies or Authorization headers
-//   })
-// );
+
+const allowedOrigins = [
+  "https://admin-test-deploy.vercel.app",
+  "http://localhost:5175", // For local development
+  "http://localhost:5176", // For local development
+];
+
+// Configure CORS
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 // api endpoints
-// readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
-app.get("/", (req, res) => res.send("API Working"));
-app.listen(port, () =>
-  console.log("Server started on PORT :" + port + ":earth_americas::wave:")
-);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on PORT : ${port}!`);
+});
